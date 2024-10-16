78a6cc17cd7aa69ccf4cefb5faf7ca4e5d718444
Hasan Alam <hasanalam365@gmail.com>
2024-10-16 14:35:47 +0600

    conditionally nav menu icon change

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BsMenuAppFill, BsMenuButtonWideFill } from 'react-icons/bs';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+
+    const [isNavOpen,setIsNavOpen]=useState(false)
+
+   
 
     const navLinks = <>
     
@@ -30,15 +35,27 @@ const Navbar = () => {
     return (
         <div className='flex items-center justify-between'>
             <div>
-                <button className='btn btn-ghost'>
-                    <img src="/full-logo-bg.png" alt="logo" className='w-[250px] h-[60px]'/>
+                <button className='btn btn-ghost '>
+                   
+                    <img src="/full-logo-bg.png" alt="logo" className='w-[200px] md:w-[250px] md:h-[60px] lg:w-[250px] lg:h-[60px]'/>
                 </button>
             </div>
             <div>
-{/* small device */}
-                <ul className='flex items-center justify-center gap-6'>
+            {/* small device */}
+                <ul className=' lg:hidden '>
+                    <button onClick={() => setIsNavOpen(!isNavOpen)}>
+                        
+                        {
+                            isNavOpen ?  <BsMenuAppFill className='text-2xl mr-3 text-yellow-600'/> :  <BsMenuButtonWideFill className='text-2xl mr-3 text-yellow-600'/>
+                        }
+                         
+                         
+                  </button>
+                </ul>
+            {/* medium/large device */}
+                <ul className='hidden md:hidden lg:flex lg:items-center lg:justify-center lg:gap-6'>
                     { navLinks}
-</ul>
+                </ul>
             </div>
         </div>
     );
