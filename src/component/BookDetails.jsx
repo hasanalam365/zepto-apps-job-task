2cde19bfb97a7a1359518a00e07cf9de1d7325e8
Hasan Alam <hasanalam365@gmail.com>
2024-10-17 11:11:25 +0600

    book details page almost done

--- src/component/BookDetails.jsx
+++ src/component/BookDetails.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaDownload, FaRegHeart } from 'react-icons/fa';
+import { LuLanguages } from 'react-icons/lu';
 import { useLoaderData } from 'react-router-dom';
 
 const BookDetails = () => {
@@ -8,26 +10,36 @@ const BookDetails = () => {
 
     return (
         <div className='flex gap-8 mt-8'>
-            <div className='w-1/2'>
+            <div className='w-[40%]'>
                  {BookData.formats["image/jpeg"] && (
                         <img 
                             src={BookData.formats["image/jpeg"]} 
                             alt={BookData.title} 
-                            className='w-full h-[500px] mb-4' 
+                            className='w-full h-[500px] mb-4 rounded-xl' 
                         />
                     )}
           </div>
-            <div>
+            <div className=' space-y-1'>
                 <h2 className='text-4xl font-bold'>{BookData.title}</h2>
                  {BookData?.authors?.map((author, idx) => (
                             <h5 key={idx}> <span className='font-medium'>Author: </span> {author.name} ({author.birth_year}- {author.death_year})</h5>
                  ))}
-                <p><span className='font-medium'>Id:</span> {BookData.id}</p>
-                {
+                <p><span className='font-medium'>Book Id:</span> {BookData.id}</p>
+               
+                <h4 className='font-medium pt-2'>Topic:</h4>
+                 {
                     BookData.subjects.map((subject, idx) => <p key={idx}>
                         {subject}
                     </p> )
                 }
+                
+                <div className='flex items-center gap-5 pt-2'>
+                    <p className='flex items-center  gap-3'> <LuLanguages /> {BookData.languages == 'en'? 'English' : ''}</p>
+                    |
+                    <p className='flex items-center  gap-3'>  <FaDownload /> {BookData.download_count}</p>
+                    |
+                     <FaRegHeart />
+                </div>
           </div>
         </div>
     );
